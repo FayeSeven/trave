8692a0a583123b0423a910c2f256f214683bd004
FayeSeven <fayeseven@163.com>
2018-05-16 14:21:15 +0800

    swiper&&data

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'//解决移动端延迟300ms问题
+import VueAwesomeSwiper from 'vue-awesome-swiper'//轮播图
 import 'styles/reset.css'
 import 'styles/border.css'//解决移动端1px边框在多倍屏幕下的问题
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,17 +1,49 @@
 <template>
     <div>
         <home-header></home-header>
+        <home-swiper :list="swiperList"></home-swiper>
     </div>
 </template>
 
 <script>
     import HomeHeader from './components/Header'
-
+    import HomeSwiper from './components/Swiper'
+    import axios from 'axios'
     export default {
         name: "Home",
         components: {
-            HomeHeader
+            HomeHeader,
+            HomeSwiper
         },
+        data () {
+            return {
+                lastCity: '',
+                swiperList: [],
+                iconList: [],
+                recommendList: [],
+                weekendList: []
+            }
+        },
+        mounted () {
+            this.lastCity = this.city
+            this.getHomeInfo()
+        },
+        methods: {
+            getHomeInfo () {
+                axios.get('/api/index.json?city=' + this.city)
+                    .then(this.getHomeInfoSucc)
+            },
+            getHomeInfoSucc (res) {
+                res = res.data
+                if (res.ret && res.data) {
+                    const data = res.data
+                    this.swiperList = data.swiperList
+                    this.iconList = data.iconList
+                    this.recommendList = data.recommendList
+                    this.weekendList = data.weekendList
+                }
+            }
+        }
     }
 </script>
 
