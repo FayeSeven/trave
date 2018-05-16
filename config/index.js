3952a074286b27da5899ec93ce793f6e5bd7557f
FayeSeven <fayeseven@163.com>
2018-05-16 16:29:04 +0800

    api

--- config/index.js
+++ config/index.js
@@ -10,7 +10,13 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+        '/api': {
+            target: 'http://localhost:7777',
+            pathRewrite: {
+                '^/api': '/static/mock'
+        }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
