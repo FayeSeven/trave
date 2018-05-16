2cf8da7640ebe8418b0faa2125b6ac1f0773dbd2
FayeSeven <fayeseven@163.com>
2018-05-16 14:49:36 +0800

    图标区域

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,20 +2,24 @@
     <div>
         <home-header></home-header>
         <home-swiper :list="swiperList"></home-swiper>
+        <home-icons :list="iconList"></home-icons>
     </div>
 </template>
 
 <script>
     import HomeHeader from './components/Header'
     import HomeSwiper from './components/Swiper'
+    import HomeIcons from './components/Icons'
     import axios from 'axios'
+
     export default {
         name: "Home",
         components: {
             HomeHeader,
-            HomeSwiper
+            HomeSwiper,
+            HomeIcons
         },
-        data () {
+        data() {
             return {
                 lastCity: '',
                 swiperList: [],
@@ -24,16 +28,22 @@
                 weekendList: []
             }
         },
-        mounted () {
+        mounted() {
             this.lastCity = this.city
             this.getHomeInfo()
         },
+        activated() {
+            if (this.lastCity !== this.city) {
+                this.lastCity = this.city
+                this.getHomeInfo()
+            }
+        },
         methods: {
-            getHomeInfo () {
+            getHomeInfo() {
                 axios.get('/api/index.json?city=' + this.city)
                     .then(this.getHomeInfoSucc)
             },
-            getHomeInfoSucc (res) {
+            getHomeInfoSucc(res) {
                 res = res.data
                 if (res.ret && res.data) {
                     const data = res.data
